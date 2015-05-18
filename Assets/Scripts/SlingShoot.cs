7e8e921851ff6d6ec58ec639610e23b534a80a3c
Patrycja Krasinski <pati.kras@web.de>
2015-05-18 16:01:22 +0200

    SlingShoot with Mouse behaviour

---
+++ Assets/Scripts/SlingShoot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingShoot : MonoBehaviour {
+	public GameObject launchPoint;
+
+	void Awake(){
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+	}
+
+	void OnMouseEnter(){
+		//print ("Mouse has entered, Sir!");
+		launchPoint.SetActive (true);
+	}
+
+	void OnMouseExit(){
+		//print ("Mouse has exited, Sir!");
+		launchPoint.SetActive (false);
+	}
+}
